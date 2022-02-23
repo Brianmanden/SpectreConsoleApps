a650e055a8e2c48813344b66c68625bff3408f27
Brian Andersen <brianmanden@gmail.com>
2022-02-23 20:43:53 +0100

    Add choices

---
+++ spectre-demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Markup("[underline red]Hello[/] World!\n");
+AnsiConsole.Markup("[green]This is all green[/]\n");
+
+var answerReadme = AnsiConsole.Confirm("Generate a [green]README[/] file?");
+var answerGitIgnore = AnsiConsole.Confirm("Generate a [yellow].gitignore[/] file?");
+
+var framework = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+            .Title("Select [green]test framework[/] to use")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Move up or down to reveal more frameworks[/])")
+            .AddChoices(
+                new[]
+                {
+                    "XUnit", "NUnit", "MSTest"
+                }
+            ));
+
+AnsiConsole.Markup($"Chosen framework was: {framework}\n");
