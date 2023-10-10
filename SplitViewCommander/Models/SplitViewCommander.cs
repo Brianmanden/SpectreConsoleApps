1cce3e3db5e543949b7d2bbb2858b1aec8fbc33d
Brian Andersen <brianmanden@gmail.com>
2023-10-10 20:18:53 +0200

    REFACTOR Main program into class ADD Styles

---
+++ SplitViewCommander/Models/SplitViewCommander.cs
@@ -0,0 +1,115 @@
+﻿using Spectre.Console;
+
+namespace Models
+{
+    /// <summary>
+    /// Main Class of the Split View Commander project.
+    /// </summary>
+    public class SplitViewCommander
+    {
+        /// <summary>
+        /// Returns zero based amount of styles defined.
+        /// </summary>
+        public int Styles => menuBarStyles.Length;
+
+        #region Styles
+        public Style[] menuBarStyles = new[]{
+                new Style(Color.DarkSlateGray1, Color.Blue3),
+                new Style(Color.DarkSlateGray2, Color.Blue1),
+                new Style(Color.DarkSlateGray3, Color.DarkBlue),
+                new Style(Color.Green, Color.Yellow),
+                new Style(Color.DarkRed, Color.DarkRed_1),
+            };
+
+        public Color menuBarBoldCharColor = Color.White;
+
+        public Style[] bodyStyles = new[] {
+                new Style(Color.DarkGreen, Color.PaleTurquoise1),
+                new Style(Color.DarkGreen, Color.PaleTurquoise1),
+                new Style(Color.DarkGreen, Color.PaleTurquoise1),
+                new Style(Color.DarkGreen, Color.PaleTurquoise1),
+                new Style(Color.DarkGreen, Color.PaleTurquoise1),
+            };
+
+        public Style[] buttonMenuStyles = new[] {
+                new Style(Color.DarkSlateGray1, Color.Blue3),
+                new Style(Color.DarkSlateGray2, Color.Blue1),
+                new Style(Color.DarkSlateGray3, Color.DarkBlue),
+                new Style(Color.Green, Color.Yellow),
+                new Style(Color.DarkRed, Color.DarkRed_1),
+            };
+
+        public Color buttonMenuBoldCharColor = Color.White;
+        #endregion
+
+        public SplitViewCommander(){}
+
+        /// <summary>
+        /// Renders the complete layout of the Split View Commander.
+        /// </summary>
+        public void RenderLayout(int styles)
+        {
+            #region MenuBar
+            var menuBarItems = new Markup[] {
+                new Markup($"[{menuBarBoldCharColor}]F[/]ile", menuBarStyles[styles]).LeftJustified(),
+                new Markup($"[{menuBarBoldCharColor}]M[/]ark", menuBarStyles[styles]).LeftJustified(),
+                new Markup($"[{menuBarBoldCharColor}]C[/]ommands", menuBarStyles[styles]).LeftJustified(),
+            };
+
+            Table menuBar = new Table();
+            menuBar.Border = TableBorder.None;
+            foreach (Markup item in menuBarItems)
+            {
+                menuBar.AddColumn(new TableColumn(item));
+            }
+            #endregion
+
+            #region Body Content
+            Table bodyContent = new Table();
+            bodyContent.Border = TableBorder.None;
+            bodyContent.Expand = true;
+            bodyContent.AddColumn("Drive/Folder 1");
+            bodyContent.AddColumn("Drive/Folder 2");
+            bodyContent.AddRow(new Rule(), new Rule());
+            bodyContent.AddRow("files", "ANOTHERFILE.EXT");
+            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
+            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
+            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
+            #endregion
+
+            #region Footer Button Bar
+            Markup[] footerButtonItems = new Markup[]{
+                new Markup($"[{buttonMenuBoldCharColor}]F3[/] View", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F4[/] Edit", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F5[/] Copy", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F6[/] Move/Rename", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F7[/] Make Folder", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F8[/] Delete file/folder", buttonMenuStyles[styles]).LeftJustified(),
+                new Markup($"[{buttonMenuBoldCharColor}]F10[/] Exit SVC", buttonMenuStyles[styles]).LeftJustified(),
+            };
+
+            Table footerMenu = new Table();
+            footerMenu.Border = TableBorder.None;
+            foreach (Markup item in footerButtonItems)
+            {
+                footerMenu.AddColumn(new TableColumn(item));
+            };
+            #endregion
+
+            #region Render Main Table
+            Table mainTable = new Table()
+                        .Centered()
+                        .Border(TableBorder.Rounded)
+                        .Centered()
+                        .Title("Split View Commander", menuBarStyles[styles])
+                        .AddColumn(new TableColumn(menuBar))
+                        .AddRow(bodyContent)
+                        .AddRow(new Rule())
+                        .AddRow(footerMenu);
+            //mainTable.UseSafeBorder = true;
+
+            AnsiConsole.Write(mainTable);
+            #endregion
+        }
+    }
+}
--- SplitViewCommander/Program.cs
+++ SplitViewCommander/Program.cs
@@ -1,106 +1,45 @@
-﻿using Spectre.Console;
+﻿using Models;
+using Spectre.Console;
 
-namespace SplitViewCommander
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        bool keepLooping = true;
+
+        var svc = new SplitViewCommander();
+
+        #region TEST Choose Styles
+        int numberOfStyles = svc.Styles;
+        string[] styleOptions = new String[numberOfStyles + 1];
+        for (int i = 0; i < numberOfStyles; i++)
         {
-            #region TableDemo
-
-            #region Styles
-            Style menuBarStyles = new Style(Color.DarkSlateGray1, Color.Blue3);
-            Color menuBarBoldCharColor = Color.White;
-
-            Style bodyStyles = new Style(Color.DarkGreen, Color.PaleTurquoise1);
-            
-            Style buttonMenuStyles = new Style(Color.DarkSlateGray1, Color.Blue3);
-            Color buttonMenuBoldCharColor = Color.White;
-            #endregion
-
-            #region MenuBar
-            var menuBarItems = new Markup[] {
-                new Markup($"[{menuBarBoldCharColor}]F[/]ile", menuBarStyles).LeftJustified(),
-                new Markup($"[{menuBarBoldCharColor}]M[/]ark", menuBarStyles).LeftJustified(),
-                new Markup($"[{menuBarBoldCharColor}]C[/]ommands", menuBarStyles).LeftJustified(),
-            };
-
-            Table menuBar = new Table();
-            menuBar.Border = TableBorder.None;
-            foreach (Markup item in menuBarItems)
-            {
-                menuBar.AddColumn(new TableColumn(item));
-            }
-            #endregion
-
-            #region Files
-            //var files = AnsiConsole.Prompt(
-            //    new MultiSelectionPrompt<string>()
-            //        .NotRequired()
-            //        .AddChoices<string>(
-            //            new[]{
-            //                "file1.ext",
-            //                "file2.ext",
-            //                "file3.ext",
-            //                "file4.ext",
-            //                "file5.ext",
-            //                "file7.ext",
-            //                "file8.ext",
-            //            }
-            //        )                
-            //);
-            #endregion
-
-            #region Body Content
-            Table bodyContent = new Table();
-            bodyContent.Border = TableBorder.None;
-            bodyContent.Expand = true;
-            bodyContent.AddColumn("Drive/Folder 1");
-            bodyContent.AddColumn("Drive/Folder 2");
-            bodyContent.AddRow(new Rule(), new Rule());
-            bodyContent.AddRow("files", "ANOTHERFILE.EXT");
-            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
-            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
-            bodyContent.AddRow("FILENAME.EXT", "ANOTHERFILE.EXT");
-            #endregion
-
-            #region Footer Button Bar
-            Markup[] footerButtonItems = new Markup[]{
-                new Markup($"[{buttonMenuBoldCharColor}]F3[/] View", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F4[/] Edit", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F5[/] Copy", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F6[/] Move/Rename", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F7[/] Make Folder", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F8[/] Delete file/folder", buttonMenuStyles).LeftJustified(),
-                new Markup($"[{buttonMenuBoldCharColor}]F10[/] Exit SVC", buttonMenuStyles).LeftJustified(),
-            };
-
-            Table footerMenu = new Table();
-            footerMenu.Border = TableBorder.None;
-            foreach (Markup item in footerButtonItems)
-            {
-                footerMenu.AddColumn(new TableColumn(item));
-            };
-            #endregion
-
-            #region Render Main Table
-            Table mainTable = new Table()
-                        .Centered()
-                        .Border(TableBorder.Rounded)
-                        .Centered()
-                        .Title("Split View Commander", menuBarStyles)
-                        .AddColumn(new TableColumn(menuBar))
-                        .AddRow(bodyContent)
-                        .AddRow(new Rule())
-                        .AddRow(footerMenu);
-            //mainTable.UseSafeBorder = true;
-
-            AnsiConsole.Write(mainTable);
-            #endregion
-
-            #endregion
-
-            Console.ReadKey();
+            styleOptions[i] = i.ToString();
         }
+        styleOptions[numberOfStyles] = "Exit";
+
+
+        while (keepLooping == true)
+        {
+            AnsiConsole.WriteLine("Chose a styleset or exit");
+            var chosenStyle = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .AddChoices(
+                        styleOptions
+                    )
+            );
+
+            if (chosenStyle == "Exit")
+            {
+                keepLooping = false;
+                AnsiConsole.WriteLine("Ok .. bye bye.");
+            }
+            else { 
+                AnsiConsole.Clear();
+                svc.RenderLayout(Convert.ToInt32(chosenStyle));
+            }
+        }
+
+        #endregion
     }
 }
