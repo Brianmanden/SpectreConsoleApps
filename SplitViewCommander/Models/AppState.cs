9871b87dfff1417f8a7ab08643f3cb3c924b784b
Brian Andersen <brianmanden@gmail.com>
2023-10-16 00:33:21 +0200

    ADD AppState Model

---
+++ SplitViewCommander/Models/AppState.cs
@@ -0,0 +1,10 @@
+﻿namespace SplitViewCommander.Models
+{
+    public class AppState
+    {
+        public Theme CurrentTheme { get; set; }
+
+        //TODO Add model describing Panels
+        public string ActivePanel { get; set; }
+    }
+}
